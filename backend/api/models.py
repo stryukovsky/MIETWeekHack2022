acf468fe9f6ced6f51d12edb25ffa0a927bbdb9f
Ярик <yar1m@mail.ru>
2022-10-22 22:57:14 +0300

    style: month

--- backend/api/models.py
+++ backend/api/models.py
@@ -15,7 +15,7 @@ class CallStatus(models.TextChoices):
     FAILED = 'Failed'
 
 
-russian_months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Августе', 'Сентябрь', 'Октябрь',
+russian_months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь',
                   'Ноябрь', 'Декабрь']
 
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -31,6 +31,7 @@ class Call(models.Model):
 class Trigger(models.Model):
     event = models.CharField(max_length=255, choices=TriggerEvent.choices, default=TriggerEvent.EXECUTE)
     call = models.OneToOneField(Call, on_delete=models.CASCADE, null=True)
+    #Dima-dzan tut dodelat nado
     #message_file = models.FileField()
 
 
