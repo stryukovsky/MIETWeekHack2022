7bb8f4fc6d956c9e0ff34c5b7632fb4a66748282
coldwithu <dima061101@gmail.com>
2022-10-23 13:06:59 +0300

    feat: CallPage form

--- frontend/src/Pages/CallPage.jsx
+++ frontend/src/Pages/CallPage.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
@@ -21,8 +17,6 @@ const innerTheme = createTheme({
 });
 
 function CallPage() {
-    // const [trigger, setTrigger] = React.useState('');
-
     const [formInput, setFormInput] = React.useReducer(
         (state, newState) => ({ ...state, ...newState }),
         {
@@ -31,25 +25,9 @@ function CallPage() {
         }
     );
 
-    // const handleChange = (event) => {
-    //     setTrigger(event.target.value);
-    // };
-
     const handleSubmit = evt => {
         evt.preventDefault();
-
-        let data = { formInput };
         axios.get(`https://dec9-91-103-248-36.ngrok.io/api/performCall?to=${formInput.to}&trigger=${formInput.trigger}`).then(response => alert(response.data.message));
-
-        // fetch(`http://5497-91-103-248-36.ngrok.io/api/performCall?to=${data.to}&trigger=${data.trigger}`, {
-        //     method: "GET",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     }
-        // })
-        //     .then(response => response.json())
-        //     .then(response => console.log("Success:", JSON.stringify(response)))
-        //     .catch(error => console.error("Error:", error));
     };
 
     const handleInput = evt => {
@@ -68,21 +46,6 @@ function CallPage() {
                             autoComplete="off"
                         >
                             <Typography textAlign="center" sx={{fontSize:22, mb: 2}}>Сделать звонок</Typography>
-                            {/*<FormControl fullWidth>*/}
-                            {/*    <InputLabel color="primary" id="demo-simple-select-label">Триггер</InputLabel>*/}
-                            {/*    <Select*/}
-
-                            {/*        labelId="demo-simple-select-label"*/}
-                            {/*        id="demo-simple-select"*/}
-                            {/*        value={trigger}*/}
-                            {/*        label="Триггер"*/}
-                            {/*        onChange={handleChange}*/}
-                            {/*    >*/}
-                            {/*        <MenuItem value={0}>Trigger 1</MenuItem>*/}
-                            {/*        <MenuItem value={2}>Trigger 2</MenuItem>*/}
-                            {/*        <MenuItem value={3}>Trigger 3</MenuItem>*/}
-                            {/*    </Select>*/}
-                            {/*</FormControl>*/}
                             <TextField
                                 color="primary"
                                 fullWidth
