15bb33ad0b8005d4c3b77228ddaa149caa981065
Dima Stryukovsky <strukovsky1@gmail.com>
2022-10-23 17:49:31 +0400

    chore: add kill VoIP

--- backend/voip/main.py
+++ backend/voip/main.py
@@ -5,7 +5,15 @@ from voip.models import VoIPConfig
 
 
 def perform_call(config: VoIPConfig, trigger: Trigger, receiver_phone: str):
-    call_string = f".\\voip\\Caller.exe {config.username} {config.password} {config.sip_server_address}" \
-                  f" {config.sip_server_port} {trigger.message_file.path} {receiver_phone}"
-    print(call_string)
-    subprocess.call(call_string)
+    call_params = [
+        ".\\voip\\Caller.exe",
+        config.username,
+        config.password,
+        config.sip_server_address,
+        config.sip_server_port,
+        trigger.message_file.path,
+        receiver_phone,
+    ]
+    process = subprocess.Popen(call_params)
+    print(call_params)
+    process.kill()
