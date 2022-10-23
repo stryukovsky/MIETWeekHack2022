a4b3c32f04a6be89873c34a09f80c985ed236eec
Dmitrii <kondim007@mail.ru>
2022-10-23 15:14:54 +0300

    feat: add Caller.exe

---
+++ Caller/CallHandlerSample.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Ozeki.Media.MediaHandlers;
+using Ozeki.VoIP;
+using Ozeki.VoIP.SDK;
+
+
+namespace ozeki.voip.sip.client
+{
+    //These classes is just an example how to use OZEKI VoIP SIP SDK, what dll is added to the References in Solution Explorer
+    //Feel free to use the other components of this SDK
+    //For More information please visit : http://www.voip-sip-sdk.com and http://www.ozekiphone.com/ozeki-voip-sip-client-997.html
+    class CallHandlerSample
+    {
+        private ISoftPhone softPhone;
+        private IPhoneLine phoneLine;
+        private RegState phoneLineInformation;
+        private IPhoneCall call;
+        private readonly Microphone microphone;
+        private readonly Speaker speaker;
+        private readonly MediaConnector connector;
+        private readonly PhoneCallAudioSender mediaSender;
+        private readonly PhoneCallAudioReceiver mediaReceiver;
+
+        /// <summary>
+        /// Event triggered when the registered softphone has called
+        /// </summary>
+        public event EventHandler<VoIPEventArgs<IPhoneCall>> IncomingCallReceived;
+
+        /// <summary>
+        /// Event the softphone has successfully registered
+        /// </summary>
+        public event EventHandler RegistrationSucceded;
+
+        /// <summary>
+        /// Handler of making call and receiving call
+        /// </summary>
+        /// <param name="registerName">The SIP ID what will registered into your PBX</param>
+        /// <param name="domainHost">The address of your PBX</param>
+        public CallHandlerSample(string registerName, string domainHost)
+        {
+            microphone = Microphone.GetDefaultDevice();
+            speaker = Speaker.GetDefaultDevice();
+            connector = new MediaConnector();
+            mediaSender = new PhoneCallAudioSender();
+            mediaReceiver = new PhoneCallAudioReceiver();
+
+            InitializeSoftPhone(registerName, domainHost);
+        }
+
+
+        /// <summary>
+        ///It initializes a softphone object with a SIP PBX, and it is for requisiting a SIP account that is nedded for a SIP PBX service. It registers this SIP
+        ///account to the SIP PBX through an ’IphoneLine’ object which represents the telephoneline. 
+        ///If the telephone registration is successful we have a call ready softphone. In this example the softphone can be reached by dialing the registername.
+        /// </summary>
+        /// <param name="registerName">The SIP ID what will registered into your PBX</param>
+        /// <param name="domainHost">The address of your PBX</param>
+        private void InitializeSoftPhone(string registerName, string domainHost)
+        {
+            try
+            {
+                softPhone = SoftPhoneFactory.CreateSoftPhone(5700, 5800);
+                softPhone.IncomingCall += softPhone_IncomingCall;
+                phoneLine = softPhone.CreatePhoneLine(new SIPAccount(true, registerName, registerName, registerName, registerName, domainHost, 5060));
+                phoneLine.RegistrationStateChanged += phoneLine_PhoneLineInformation;
+
+                softPhone.RegisterPhoneLine(phoneLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("You didn't give your local IP adress, so the program won't run properly.\n {0}", ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Create and start the call to the dialed number
+        /// </summary>
+        /// <param name="dialedNumber"></param>
+        public void Call(string dialedNumber)
+        {
+            if (phoneLineInformation != RegState.RegistrationSucceeded)
+            {
+                Console.WriteLine("Phone line state is not valid!");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(dialedNumber))
+                return;
+
+            if (call != null)
+                return;
+
+            call = softPhone.CreateCallObject(phoneLine, dialedNumber);
+            WireUpCallEvents();
+            call.Start();
+        }
+
+        /// <summary>
+        /// Occurs when phone line state has changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void phoneLine_PhoneLineInformation(object sender, RegistrationStateChangedArgs e)
+        {
+            phoneLineInformation = e.State;
+            Console.WriteLine("Register name:" + ((IPhoneLine)sender).SIPAccount.RegisterName);
+
+            if (e.State == RegState.RegistrationSucceeded)
+            {
+                Console.WriteLine("Registration succeeded. Online.");
+                OnRegistrationSucceded();
+            }
+            else
+            {
+                Console.WriteLine("Current state:" + e.State);
+            }
+        }
+
+        /// <summary>
+        /// Occurs when an incoming call request has received.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void softPhone_IncomingCall(object sender, VoIPEventArgs<IPhoneCall> e)
+        {
+            Console.WriteLine("Incoming call from {0}", e.Item.DialInfo);
+            call = e.Item;
+            WireUpCallEvents();
+            OnIncomingCallReceived(e.Item);
+        }
+
+        /// <summary>
+        /// Occurs when the phone call state has changed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void call_CallStateChanged(object sender, CallStateChangedArgs e)
+        {
+            Console.WriteLine("Call state changed: " + e.State);
+
+            switch (e.State)
+            {
+                case CallState.InCall:
+                    ConnectDevicesToCall();
+                    break;
+                case CallState.Completed:
+                    DisconnectDevicesFromCall();
+                    WireDownCallEvents();
+                    call = null;
+                    break;
+                case CallState.Cancelled:
+                    WireDownCallEvents();
+                    call = null;
+                    break;
+            }
+        }
+
+        private void OnRegistrationSucceded()
+        {
+            var handler = RegistrationSucceded;
+
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+
+        private void OnIncomingCallReceived(IPhoneCall item)
+        {
+            var handler = IncomingCallReceived;
+
+            if (handler != null)
+                handler(this, new VoIPEventArgs<IPhoneCall>(item));
+        }
+
+        /// <summary>
+        /// Connecting the microphone and speaker to the call
+        /// </summary>
+        private void ConnectDevicesToCall()
+        {
+            if (microphone != null)
+                microphone.Start();
+            connector.Connect(microphone, mediaSender);
+
+            if (speaker != null)
+                speaker.Start();
+            connector.Connect(mediaReceiver, speaker);
+
+            mediaSender.AttachToCall(call);
+            mediaReceiver.AttachToCall(call);
+        }
+
+        /// <summary>
+        /// Disconnecting the microphone and speaker from the call
+        /// </summary>
+        private void DisconnectDevicesFromCall()
+        {
+            if (microphone != null)
+                microphone.Stop();
+            connector.Disconnect(microphone, mediaSender);
+
+            if (speaker != null)
+                speaker.Stop();
+            connector.Disconnect(mediaReceiver, speaker);
+
+            mediaSender.Detach();
+            mediaReceiver.Detach();
+        }
+
+        /// <summary>
+        ///  It signs up to the necessary events of a call transact.
+        /// </summary>
+        private void WireUpCallEvents()
+        {
+            if (call != null)
+            {
+                call.CallStateChanged += ( call_CallStateChanged );
+            }
+        }
+
+        /// <summary>
+        /// It signs down from the necessary events of a call transact.
+        /// </summary>
+        private void WireDownCallEvents()
+        {
+            if (call != null)
+            {
+                call.CallStateChanged -= (call_CallStateChanged);
+            }
+        }
+
+        ~CallHandlerSample()
+        {
+            if (softPhone != null)
+                softPhone.Close();
+            WireDownCallEvents();
+        }
+    }
+}
---
+++ Caller/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Ozeki.Media.MediaHandlers;
+using Ozeki.VoIP;
+using Ozeki.VoIP.SDK;
+
+namespace Caller
+{
+    class Program
+    {
+        static ISoftPhone softphone;
+        static IPhoneLine phoneLine;
+        static IPhoneCall call;
+        static Microphone microphone;
+        static Speaker speaker;
+        static MediaConnector connector;
+        static PhoneCallAudioSender mediaSender;
+        static PhoneCallAudioReceiver mediaReceiver;
+        static WaveStreamPlayback player;
+
+        static string addresseePhone;
+
+
+        // args = [ "userName", "password", "domainHost", domainPortIntNumber, "absolutePathToWaveFile",  "addresseePhone" ]
+        private static void Main(string[] args)
+        {
+            // Create a softphone object with RTP port range 5000-10000
+            softphone = SoftPhoneFactory.CreateSoftPhone(5000, 10000);
+
+            string userName, registerPassword, domainHost;
+            int domainPort;
+            SIPAccount account;
+
+            if (args.Length != 6)
+            {
+                var registrationRequired = false;
+                userName = "user9";
+                var displayName = userName;
+                var authenticationId = userName;
+                registerPassword = "SR6hYqjo";
+                domainHost = "vpbx400135286.mangosip.ru";
+                domainPort = 5060;
+                // TODO: поменять пуль на относительный
+                player = new WaveStreamPlayback(@"C:\Users\kondi\Desktop\file_example_WAV_1MG.wav");
+                addresseePhone = "5559201";
+
+                account = new SIPAccount(registrationRequired, displayName, userName, authenticationId, registerPassword, domainHost, domainPort);
+            }
+            else
+            {
+                var registrationRequired = false;
+                userName = args[0];
+                var displayName = userName;
+                var authenticationId = userName;
+                registerPassword = args[1];
+                domainHost = args[2];
+                domainPort = int.Parse(args[3]);
+                player = new WaveStreamPlayback(args[4]);
+                addresseePhone = args[5];
+
+                account = new SIPAccount(registrationRequired, displayName, userName, authenticationId, registerPassword, domainHost, domainPort);
+            }
+
+            // Send SIP regitration request
+            RegisterAccount(account);
+
+            mediaSender = new PhoneCallAudioSender();
+            mediaReceiver = new PhoneCallAudioReceiver();
+            connector = new MediaConnector();
+
+            Console.ReadLine();
+        }
+
+        static void RegisterAccount(SIPAccount account)
+        {
+            try
+            {
+                phoneLine = softphone.CreatePhoneLine(account);
+                phoneLine.RegistrationStateChanged += line_RegStateChanged;
+                softphone.RegisterPhoneLine(phoneLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error during SIP registration: " + ex);
+            }
+        }
+
+        static void line_RegStateChanged(object sender, RegistrationStateChangedArgs e)
+        {
+            if (e.State == RegState.NotRegistered || e.State == RegState.Error)
+                Console.WriteLine("Registration failed!");
+
+            if (e.State == RegState.RegistrationSucceeded)
+            {
+                Console.WriteLine("Registration succeeded - Online!");
+                CreateCall();
+            }
+        }
+
+        private static void CreateCall()
+        {
+            var numberToDial = addresseePhone;
+            call = softphone.CreateCallObject(phoneLine, numberToDial);
+            call.CallStateChanged += call_CallStateChanged;
+            call.Start();
+        }
+
+        private static void SetupDevices()
+        {
+            connector.Connect(microphone, mediaSender);
+            connector.Connect(mediaReceiver, speaker);
+
+            mediaSender.AttachToCall(call);
+            mediaReceiver.AttachToCall(call);
+
+            microphone.Start();
+            speaker.Start();
+        }
+
+        static void SetupPlayer()
+        {
+            connector.Connect(player, mediaSender);
+            mediaSender.AttachToCall(call);
+
+            player.Start();
+
+            Console.WriteLine("The wav player is streaming.");
+        }
+
+
+        static void call_CallStateChanged(object sender, CallStateChangedArgs e)
+        {
+            Console.WriteLine("Call state: {0}.", e.State);
+
+            if (e.State == CallState.Answered)
+                SetupPlayer();
+        }
+    }
+}
